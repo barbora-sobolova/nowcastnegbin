29654c5dc870ce92d1d4fd8588bbe2710005b8e6
Barbora Němcová <barbora.sobolova@kit.edu>
2025-12-19 09:00:35 +0100

    Change the inequality signs to less/greater than or equal when calculating the coverage

--- R/plot.R
+++ R/plot.R
@@ -164,11 +164,11 @@ plot_coverage <- function(
     group_by(.data$delay, .data$Distribution) |>
     summarize(
       coverage_50 = sum(
-        .data$true_val > .data$quantile_25 & .data$true_val < .data$quantile_75
+        .data$true_val >= .data$quantile_25 & .data$true_val <=.data$quantile_75
       ) / n(),
       coverage_95 = sum(
-        .data$true_val > .data$quantile_2.5 &
-          .data$true_val < .data$quantile_97.5
+        .data$true_val >= .data$quantile_2.5 &
+          .data$true_val <= .data$quantile_97.5
       ) / n(),
     ) |>
     ungroup() |>
