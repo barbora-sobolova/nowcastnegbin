018f054f4b1cfaca8d5c483affd31edd5e1e587f
Barbora Němcová <barbora.sobolova@kit.edu>
2026-04-08 09:29:58 +0200

    Clarify parameter documentation

--- R/prepare_data.R
+++ R/prepare_data.R
@@ -159,7 +159,9 @@ mock_unobserved <- function(obs_counts) {
 #' @param prior_delay_param a vector of positive real values, parameters of the
 #' prior Dirichlet distribution of the reporting delay. The higher the sum of
 #' its elements is, the more informative the prior distribution of the reporting
-#' delay becomes.
+#' delay becomes. If NULL, the NULL value will be propagated to the output,
+#' which is fine for fitting the GLM model, but for the STAN model,
+#' \code{prior_delay_param} must be specified.
 #' @param skip_rows indices of rows, corresponding to the  dates, on which we
 #' don't calculate the nowcast due to Christmas. These are used to calculate the
 #' specific reporting pattern of the Christmas period.
