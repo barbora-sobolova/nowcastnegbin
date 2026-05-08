16e9dfb6fb82172da5eb18334c1809f84e14a1fd
Barbora Němcová <barbora.sobolova@kit.edu>
2026-05-08 11:50:25 +0200

    Add a safeguard against invalid values of the Dirichlet prior params

--- R/prepare_data.R
+++ R/prepare_data.R
@@ -194,8 +194,13 @@ get_stan_data <- function(
   max_lag <- ncol(train_data)
   # Make sure that the parameters of the prior distribution of the delay
   # probabilities have a correct length.
-  if (!is.null(prior_delay_param) && length(prior_delay_param) != max_lag) {
-    stop("The vector of prior parameters of the reporting delay must have the same length as there are columns in the reporting triangle.")  # nolint
+  if (!is.null(prior_delay_param)) {
+    if (length(prior_delay_param) != max_lag) {
+      stop("The vector of prior parameters of the reporting delay must have the same length as there are columns in the reporting triangle.")  # nolint
+    }
+    if (!is.numeric(prior_delay_param) || any(prior_delay_param <= 0)) {
+      stop("`prior_delay_param` must be a numeric vector of strictly positive finite values.")  # nolint
+    }
   }
   # Replace the known counts by NAs to create the reporting triangle
   obs_mat_truncated <- mock_unobserved(train_data)
